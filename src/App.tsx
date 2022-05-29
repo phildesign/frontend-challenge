c83dacb3d3f3db61f3405fb8323171cbf4cfb66e
Phil <phil.nice.23@gmail.com>
2022-05-30 01:18:41 +0300

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import './App.module.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Favorites from './pages/Favorites/Favorites';
+import cn from 'classnames';
+
+import styles from './App.module.css';
 
 const App = () => {
-	return <div className="App"></div>;
+	return (
+		<div className={styles.app}>
+			<main className={cn(styles.main, styles.container)}>
+				<Routes>
+					<Route path="/home" element={<Home />} />
+					<Route path="/favorites" element={<Favorites />} />
+				</Routes>
+			</main>
+		</div>
+	);
 };
 
 export default App;
---
+++ src/pages/Favorites/Favorites.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Favorites.module.css';
+
+const Favorites = () => {
+	return <div className={styles.favorites}>Favorites</div>;
+};
+
+export default Favorites;
